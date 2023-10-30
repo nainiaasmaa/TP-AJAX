028111b21622c7bba7ebcec5a28410bdd605e34b
nainiaasmaa <147659638+nainiaasmaa@users.noreply.github.com>
2023-10-30 22:46:28 +0100

    Add files via upload

---
+++ build/web/script/machineByMarque.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    
+    $("#marque").change(function () {
+            $.ajax ({
+            url: 'MachineByMarqueController',
+            data: {m:  $(this).val()},//this = élément sélectionné
+            type: 'POST',
+            success: function (data) {
+                var ligne="";
+                for (var i = 0; i < data.length; i++) {
+                    ligne+='<tr>'
+                            +'<td>'+data[i].id+'</td>'
+                            +'<td>'+data[i].reference+'</td>'
+                            +'<td>'+data[i].prix+'</td>'
+                            +'<td>'+data[i].dateAchat+'</td>'
+                            +'<td>'+data[i].marque.libelle+'</td>'
+                            +'<td><a class="bndelete" href="MachineController?op=delete&id='+data[i].id+'">Supprimer</a></td>'
+                            +'<td><a class="bnupdate" href="">Modifier</a></td>'
+                        +'</tr>';
+                }
+                $("#body").html(ligne);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log(textStatus);
+            }
+        });
+    });
+
+
+});
+
